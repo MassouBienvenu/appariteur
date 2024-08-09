de927baef8a8cda81d988b51a8f59c7ab0a396cc
MassouBienvenu <massoubienvenu@gmail.com>
2024-08-09 11:11:07 +0100

    update

--- lib/controllers/fichepaieController/fichePaieChild.dart
+++ lib/controllers/fichepaieController/fichePaieChild.dart
@@ -1,13 +1,18 @@
 import 'dart:io';
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:flutter_staggered_animations/flutter_staggered_animations.dart';
+import 'package:open_file/open_file.dart';
 import 'package:syncfusion_flutter_pdfviewer/pdfviewer.dart';
 import 'package:flutter_file_downloader/flutter_file_downloader.dart';
 import 'package:path_provider/path_provider.dart';
 import '../../data/apihelper.dart';
+import '../../main.dart';
 import '../../models/fichepaie.dart';
 import 'package:flutter_downloader/flutter_downloader.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+
 class FichesPaieChild extends StatefulWidget {
   @override
   _FichesPaieChildState createState() => _FichesPaieChildState();
@@ -19,17 +24,41 @@ class _FichesPaieChildState extends State<FichesPaieChild> {
   @override
   void initState() {
     super.initState();
+    initializeNotifications();
     loadFichesPaie();
   }
 
-  Future<void> loadFichesPaie() async {
-    FlutterDownloader.initialize(
+  void initializeNotifications() {
+    const AndroidInitializationSettings initializationSettingsAndroid =
+    AndroidInitializationSettings('app_icon'); // Replace 'app_icon' with your icon name
+
+    const DarwinInitializationSettings initializationSettingsIOS =
+    DarwinInitializationSettings();
+    const InitializationSettings initializationSettings =
+    InitializationSettings( android: initializationSettingsAndroid,iOS: initializationSettingsIOS);
+
+    flutterLocalNotificationsPlugin.initialize(
+      initializationSettings,
+      onDidReceiveNotificationResponse: _onDidReceiveNotificationResponse,
     );
+  }
+
+  void _onDidReceiveNotificationResponse(NotificationResponse response) {
+    final String? payload = response.payload;
+    if (payload != null) {
+      OpenFile.open(payload);
+    }
+  }
+
+  Future<void> loadFichesPaie() async {
+    FlutterDownloader.initialize();
     final loadedFichesPaie = await AuthApi.getFichesPaie();
     setState(() {
       fichesPaie = loadedFichesPaie;
     });
   }
+
+
 
   @override
   Widget build(BuildContext context) {
@@ -78,7 +107,10 @@ class ListItem extends StatelessWidget {
   final double width;
   final FichePaie fiche;
 
-  Future<void> downloadFile(String url, String fileName, BuildContext context) async {
+  ListItem({required this.width, required this.fiche});
+
+  Future<void> downloadFile(
+      String url, String fileName, BuildContext context) async {
     final directory = await getApplicationDocumentsDirectory();
     final savePath = '${directory.path}/$fileName';
 
@@ -92,7 +124,8 @@ class ListItem extends StatelessWidget {
           onDownloadCompleted: (String path) {
             ScaffoldMessenger.of(context).showSnackBar(
               SnackBar(
-                content: Text('Téléchargement terminé avec succès. Fichier sauvegardé dans : $path'),
+                content: Text(
+                    'Téléchargement terminé avec succès. Fichier sauvegardé dans : $path'),
                 duration: Duration(seconds: 6),
               ),
             );
@@ -104,9 +137,13 @@ class ListItem extends StatelessWidget {
           if (received == total) {
             ScaffoldMessenger.of(context).showSnackBar(
               SnackBar(
-                content: Text('Fichier "$fileName" téléchargé avec succès dans : $savePath.'),
+                showCloseIcon: true,
+                  duration: Duration(seconds: 8),
+                content: Text(
+                    'Fichier "$fileName" téléchargé avec succès dans : $savePath.'),
               ),
             );
+            _showDownloadNotification(fileName, savePath);
           }
         });
       }
@@ -119,8 +156,26 @@ class ListItem extends StatelessWidget {
     }
   }
 
+  Future<void> _showDownloadNotification(
+      String fileName, String filePath) async {
+    const DarwinNotificationDetails iosPlatformChannelSpecifics =
+    DarwinNotificationDetails(
+      presentAlert: true,
+      presentBadge: true,
+      presentSound: true,
+    );
 
-  ListItem({required this.width, required this.fiche});
+    const NotificationDetails platformChannelSpecifics =
+    NotificationDetails(iOS: iosPlatformChannelSpecifics);
+
+    await flutterLocalNotificationsPlugin.show(
+      0,
+      'Téléchargement terminé',
+      'Fichier "$fileName" téléchargé avec succès. Ouvrir ?',
+      platformChannelSpecifics,
+      payload: filePath,
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -137,7 +192,8 @@ class ListItem extends StatelessWidget {
           onTap: () => Navigator.push(
             context,
             MaterialPageRoute(
-              builder: (context) => PDFScreen(url: 'https://appariteur.com/admins/documents/${fiche.fichier}'),
+              builder: (context) => PDFScreen(
+                  url: 'https://appariteur.com/admins/documents/${fiche.fichier}'),
             ),
           ),
           child: Padding(
@@ -150,33 +206,47 @@ class ListItem extends StatelessWidget {
                     Icon(Icons.picture_as_pdf, color: Colors.redAccent),
                     SizedBox(width: 10),
                     Expanded(
-                      child: Text('${fiche.mois} ${fiche.annee}', style: Theme.of(context).textTheme.titleMedium),
+                      child: Text('${fiche.mois} ${fiche.annee}',
+                          style: Theme.of(context).textTheme.titleMedium),
                     ),
                     Icon(Icons.check_circle, color: Colors.green)
                   ],
                 ),
                 SizedBox(height: 10),
-                Text('Créée le: ${fiche.dateCreate}', style: Theme.of(context).textTheme.bodyMedium),
+                Text('Créée le: ${fiche.dateCreate}',
+                    style: Theme.of(context).textTheme.bodyMedium),
                 SizedBox(height: 20),
                 Row(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
                     TextButton.icon(
                       onPressed: () {
-                        downloadFile('https://appariteur.com/admins/documents/${fiche.fichier}', fiche.fichier, context);
+                        downloadFile(
+                            'https://appariteur.com/admins/documents/${fiche.fichier}',
+                            fiche.fichier,
+                            context);
                       },
-                      icon: Icon(Icons.download, color: Theme.of(context).primaryColor),
-                      label: Text('Télécharger', style: Theme.of(context).textTheme.bodyMedium),
+                      icon: Icon(Icons.download,
+                          color: Theme.of(context).primaryColor),
+                      label: Text('Télécharger',
+                          style: Theme.of(context).textTheme.bodyMedium),
                       style: TextButton.styleFrom(
-                        padding: EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
-                        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
-                        backgroundColor: Theme.of(context).colorScheme.secondary.withOpacity(0.1),
+                        padding: EdgeInsets.symmetric(
+                            vertical: 8.0, horizontal: 16.0),
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(20)),
+                        backgroundColor: Theme.of(context)
+                            .colorScheme
+                            .secondary
+                            .withOpacity(0.1),
                       ),
                     ),
                     Row(
                       children: [
-                        Text('Voir ', style: Theme.of(context).textTheme.bodyMedium),
-                        Icon(Icons.remove_red_eye, color: Theme.of(context).primaryColor),
+                        Text('Voir ',
+                            style: Theme.of(context).textTheme.bodyMedium),
+                        Icon(Icons.remove_red_eye,
+                            color: Theme.of(context).primaryColor),
                       ],
                     )
                   ],
--- lib/main.dart
+++ lib/main.dart
@@ -5,10 +5,18 @@ import 'package:flutter_downloader/flutter_downloader.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'controllers/loginController/childlogin.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+
+
+final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await FlutterDownloader.initialize();
+  const AndroidInitializationSettings initializationSettingsAndroid=AndroidInitializationSettings('@mipmap/ic_launcher');
+  const DarwinInitializationSettings initializationSettingsiOs = DarwinInitializationSettings();
+  final InitializationSettings initializationSettings = InitializationSettings(android:initializationSettingsAndroid, iOS:initializationSettingsiOs);
+  await flutterLocalNotificationsPlugin.initialize(initializationSettings);
   SharedPreferences prefs = await SharedPreferences.getInstance();
 
   bool seenOnboarding = prefs.getBool('seenOnboarding') ?? false;
